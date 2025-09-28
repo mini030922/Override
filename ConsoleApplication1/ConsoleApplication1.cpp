9c6df82a73c0a196bd4307e51ce5d43595b7a9d1
sm092 <sm092@DESKTOP-DQ0BO2H>
2025-09-28 15:36:24 +0900

    초기 커밋.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    virtual void speak() {
+        cout << "동물이 소리를 냅니다." << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void speak() override {
+        cout << "멍멍!" << endl;
+    }
+};
+
+class Cat : public Animal {
+public:
+    void speak() override {
+        cout << "야옹!" << endl;
+    }
+};
+
+int main() {
+
+    Animal a1;
+    Dog a2;
+    Cat a3;
+
+    a1.speak();
+    a2.speak();
+    a3.speak();
+
+    Animal* a4 = &a2;
+    a4->speak();
+
+    return 0;
+}
